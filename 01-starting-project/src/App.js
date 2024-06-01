70aa9ec49cabeec4e171e072c4e8d8c7087f51d9
Kibasa <miguelhmsantos21@gmail.com>
2024-06-01 15:02:46 -0300

    setting everything up

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,5 +1,13 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />}, //main page, slash nothing
+]);//Routes are simply paths <=> components mapping;
+//So for which path (like "/products") should which component (like "<Products />") be loaded?
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
